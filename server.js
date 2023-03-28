d2b45358fcf9294b7ba676541562d5040a11bce1
Earzola34 <sailorarzola@gmail.com>
2023-03-27 19:58:59 -0600

    deliverable done

--- server.js
+++ server.js
@@ -1,23 +1,36 @@
 const express = require('express');
 
-const drinks = require('./models/drinks')
-
 const app = express();
+
+const drinks = require('./models/drinks');
+
+const foods = require('./models/food');
 
 const PORT = 3000;
 
+app.set('view engine', 'ejs');
+
 app.get('/', (req, res) => {
     res.send('Welcome to the Gitpub App!');
-});
+})
 
 app.get('/drinks', (req, res) => {
-    res.render('drinks_index.ejs', {drinks});
-});
+    res.render('drinks_index', {drinks});
+})
 
 app.get('/drinks/:id', (req, res) => {
     let drink = drinks[req.params.id];
-    res.render('drinks_show.ejs', {drink});
-});
+    res.render('drinks_show', {drink});
+})
+
+app.get('/foods', (req, res) => {
+    res.render('food_index', {foods});
+})
+
+app.get('/foods/:id', (req, res) => {
+    let food = foods[req.params.id];
+    res.render('food_show', {food});
+})
 
 app.listen (PORT, () => {
     console.log('listening on port 3000');
